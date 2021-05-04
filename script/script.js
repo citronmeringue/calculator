cad8114f0eddcf18a943d70eaad19f9e47bd36b5
angela <p.angela9292@gmail.com>
2021-05-04 13:43:43 +0200

    correction ES6 syntax

--- script/script.js
+++ script/script.js
@@ -3,29 +3,29 @@ const btnBackspace = document.getElementById('del');
 const btnEqual = document.getElementById('equal');
 const btnDecimal = document.getElementById('decimal');
 const displayContent = document.getElementById('display');
-
 const boxBody = document.querySelector('#body-calc');
 const btnNumbers = boxBody.querySelectorAll('.numbers');
 const btnOperators = document.querySelectorAll('.operator');
 
 displayContent.value = "0"
-var first_op = null;
-var operator = null;
-var second_op = null;
+let first_op = null;
+let operator = null;
+let second_op = null;
 
 
-btnBackspace.addEventListener('click', e=>{
-    if(displayContent.value.length > 1){
+btnBackspace.addEventListener('click', e => {
+    if (displayContent.value.length > 1) {
         displayContent.value = displayContent.value.slice(0, -1);
-    }else{
+    } else {
         displayContent.value = 0
     }
 });
+
 btnNumbers.forEach((button) => {
-    button.addEventListener('click', e=> {   
+    button.addEventListener('click', e => {
         btnOperators.forEach(e => e.removeAttribute("disabled"))
 
-        if (displayContent.value == 0 || (operator !==  null && displayContent.value == first_op)){
+        if (displayContent.value == 0 || (operator !== null && displayContent.value == first_op)) {
             displayContent.value = "";
         }
         displayContent.value += button.id;
@@ -35,26 +35,26 @@ btnNumbers.forEach((button) => {
 
 
 btnOperators.forEach((button) => {
-    button.addEventListener('click', e=> { 
-        if(first_op === null && second_op === null){
+    button.addEventListener('click', e => {
+        if (first_op === null && second_op === null) {
             first_op = Number(displayContent.value);
             operator = button.innerHTML;
             return
         }
 
         second_op = displayContent.value;
-        output = operate(Number(first_op), Number(second_op), operator);      
-        operator = button.innerHTML;  
-        
-        displayContent.value = output; 
+        output = operate(Number(first_op), Number(second_op), operator);
+        operator = button.innerHTML;
+
+        displayContent.value = output;
         first_op = output
 
         btnOperators.forEach(e => e.setAttribute("disabled", "disabled"));
     });
 });
 
-btnEqual.addEventListener('click', e=> { 
-    if(first_op === null && second_op === null){
+btnEqual.addEventListener('click', e => {
+    if (first_op === null && second_op === null) {
         displayContent.value;
         return
     }
@@ -64,63 +64,53 @@ btnEqual.addEventListener('click', e=> {
 
     output = operate(first_op, second_op, operator);
     displayContent.value = output;
-    btnEqual.setAttribute("disabled","disabled");
-    btnBackspace.setAttribute("disabled","disabled");
+    btnEqual.setAttribute("disabled", "disabled");
+    btnBackspace.setAttribute("disabled", "disabled");
 });
 
-btnDecimal.addEventListener('click', e=> {
-    if (!displayContent.value.includes('.')){
+btnDecimal.addEventListener('click', e => {
+    if (!displayContent.value.includes('.')) {
         displayContent.value += btnDecimal.innerText;
     }
 });
 
-btnClear.addEventListener('click',clear);
-
-function clear(){
+btnClear.addEventListener('click', e => {
     btnEqual.removeAttribute("disabled");
     btnBackspace.removeAttribute("disabled");
     displayContent.value = 0;
+    reset()
+});
+
+
+const reset = () => {
     first_op = null;
     second_op = null;
     operator = null;
-};
-
-function reset(){
-    first_op = null;
-    second_op = null;
-    operator = null;
 }
 
 
-function add(a,b){
-    return a + b;
+const add = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b == 0) throw displayContent.value = "Can't be divided by zero !";
+    return a / b;
 }
 
-function substract(a,b){
-    return a - b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    if(b == 0) throw displayContent.value = "Can't be divided by zero !";
-    return a/b;
-}
-
-function operate(a,b, operator){
-    switch(operator){
+const operate = (a, b, operator) => {
+    switch (operator) {
         case "+":
-            return add(a,b);
+            return add(a, b);
 
         case "-":
-            return substract(a,b);
+            return substract(a, b);
 
         case "*":
-            return multiply(a,b);
+            return multiply(a, b);
 
         case "/":
-            return divide(a,b);
+            return divide(a, b);
     }
 }
